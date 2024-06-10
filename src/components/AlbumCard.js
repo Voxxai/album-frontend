03cccd9912325d5d9189465c2f63c9ece9ce22e4
Voxxai <gilkranendonk@gmail.com>
2024-06-10 18:50:46 +0200

    Update AlbumCard component to improve image display and add ellipsis to title

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Card, CardHeader, CardMedia, Avatar, } from '@mui/material';
+import { Card, CardHeader, CardMedia, Avatar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const AlbumCard = ({ id, name, artist, imageUrl }) => {
   return (
-    <Link to ={`/albums/${id}`}>
+    <Link to={`/albums/${id}`}>
       <Card>
         <CardHeader
           avatar={<Avatar alt={artist} src={imageUrl} />}
-          title={name}
+          title={
+            <Typography variant="body1" noWrap sx={{ textOverflow: 'ellipsis', overflow: 'hidden' }}>
+              {name}
+            </Typography>
+          }
           subheader={artist}
         />
         <CardMedia

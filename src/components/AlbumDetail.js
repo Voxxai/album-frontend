0d2dfbde99eab229d3ba16956b6b42c019ba7ce2
Voxxai <gilkranendonk@gmail.com>
2024-06-10 18:35:38 +0200

    oeps verkeerde endpoint

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -18,7 +18,7 @@ function AlbumDetail() {
       });
 
       if (response.ok) {
-        navigate(`/albums/${id}`); // Herlaad de pagina na succesvolle update
+        navigate(`/Album/${id}`); // Herlaad de pagina na succesvolle update
       } else {
         throw new Error('Album bijwerken mislukt');
       }

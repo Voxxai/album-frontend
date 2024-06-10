046f9d0a25ae523b4ab7553b215f0cdf7cbf86f9
Voxxai <gilkranendonk@gmail.com>
2024-06-10 18:53:35 +0200

    woops avatar stonf nog in material

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardMedia, Avatar, Typography } from '@mui/material';
+import { Card, CardHeader, CardMedia, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const AlbumCard = ({ id, name, artist, imageUrl }) => {
